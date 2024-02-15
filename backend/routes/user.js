40b08f7c91103267e56d17a18013bb5230ec3f15
RAHUL <rahulnaval157821@gmail.com>
2024-02-16 01:19:12 +0530

    reset password controller

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,6 +8,11 @@ const {
   changePassword,
 } = require("../controllers/Auth");
 
+const {
+  resetPassword,
+  resetPasswordToken,
+} = require("../controllers/resetPassword");
+
 const { auth } = require("../middleware/auth");
 
 // Routes for Login, Signup, and Authentication
@@ -27,6 +32,16 @@ router.post("/sendotp", sendotp);
 
 // Route for Changing the password
 router.post("/changepassword", auth, changePassword);
+
+// ********************************************************************************************************
+//                                      Reset Password
+// ********************************************************************************************************
+
+// Route for generating a reset password token
+router.post("/reset-password-token", resetPasswordToken);
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword);
 
 // Export the router for use in the main application
 module.exports = router;
