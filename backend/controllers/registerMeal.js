b94f493a7844eb5cea1b26fd075ca3d00c74710b
anunadhu1234 <ag952006@student.nitw.ac.in>
2024-03-14 21:32:07 +0530

    registered meal completed

--- backend/controllers/registerMeal.js
+++ backend/controllers/registerMeal.js
@@ -2,28 +2,97 @@ const User = require("../models/User");
 const Meals = require("../models/Meals");
 
 exports.registerMeal = async (req, res) => {
-  try {
-    let creditPoints = req.body.creditPoints;
-    console.log(creditPoints);
+  // try {
+  //   let creditPoints = req.body.creditPoints;
+  //   console.log(creditPoints);
 
-    if (creditPoints < 1) {
-      return res.status(400).json({
+  //   if (creditPoints < 1) {
+  //     return res.status(400).json({
+  //       success: false,
+  //       message: `You don't have enough token`,
+  //     });
+  //   }
+
+  //   creditPoints--;
+  //   console.log(creditPoints);
+
+  //   return res
+  //     .status(200)
+  //     .json({ success: true, message: "Meal Registered Succ." });
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(500).json({
+  //     success: false,
+  //     error: error.message,
+  //   });
+  // }
+
+
+  try{
+    const userDetails = await User.findById(req.user.id);
+    const userEmail= userDetails.email;
+const {mealType,mealPrice} = req.body;
+const date = new Date();
+const findUser =  await Meals.findOne({userEmail});
+console.log(findUser.email," ",findUser.mealType);
+if(findUser){
+    if(findUser.mealType==mealType){
+    return res.status(400).json({
         success: false,
-        message: `You don't have enough token`,
-      });
-    }
+        message: "You have already registered for meal"
+    });
+}
+}
 
-    creditPoints--;
-    console.log(creditPoints);
+let credits = userDetails.creditPoints;
+if(credits<mealPrice){
+    return res.status(401).json({success: false, message:"Not enough credits to buy meal"});
+}
 
-    return res
-      .status(200)
-      .json({ success: true, message: "Meal Registered Succ." });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
+credits=credits- mealPrice;
+const updatedUserDetails = await User.findByIdAndUpdate(
+    req.user.id,
+    {creditPoints: credits},
+    {new:true}
+);
+
+const newRegisteredMeal = await Meals.create(
+    {email: userDetails.email,
+        dateOfMeal: date.getDate(),
+        mealType: mealType,
+        mealPrice: mealPrice}
+);
+try{
+    const emailResponse = await mailSender(
+        updatedUserDetails.email,
+        "Meal Registered Successfully",
+        registered(
+            updatedUserDetails.email,
+            `Meal is registered for ${updatedUserDetails.firstName} ${updatedUserDetails.lastName} successfully`
+        )
+    );
+    console.log("Email sent successfully:", emailResponse.response);
+}
+catch(error){
+    console.error("Error occurred while sending mail:", error);
+    return res.status(400).json({
       success: false,
+      message: "Error occurred while sending mail",
       error: error.message,
     });
-  }
+}
+
+return res.status(200).json({
+    success: true,
+    message:"Meal is Registered Successfully"
+});
+}
+catch(error){
+    console.log("Error occurred while registering for meal",error);
+    return res.status(500).json({
+        success:false,
+        message:"Error occurred while registering for meal",
+        error: error.message
+    });
+}
 };
