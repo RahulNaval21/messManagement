d6635e9bc7e4602d19679ca01266744a66f8ef3e
RAHUL <rahulnaval157821@gmail.com>
2024-03-18 18:50:31 +0530

    payment backend ( partial )

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {body, validationResult} = require('express-validator');
+const { body, validationResult } = require("express-validator");
 const {
   login,
   signup,
@@ -16,6 +16,7 @@ const {
 const { auth } = require("../middleware/auth");
 
 const { registerMeal } = require("../controllers/registerMeal");
+const { capturePayment, verifyPayment } = require("../controllers/payment");
 
 // Routes for Login, Signup, and Authentication
 
@@ -24,20 +25,35 @@ const { registerMeal } = require("../controllers/registerMeal");
 // ********************************************************************************************************
 
 // Route for user login
-router.post("/login",[body('email','Please enter a valid email').isEmail(),
-body('password','Password cannot be empty').exists()], login);
+router.post(
+  "/login",
+  [
+    body("email", "Please enter a valid email").isEmail(),
+    body("password", "Password cannot be empty").exists(),
+  ],
+  login
+);
 
 // Route for user signup
-router.post("/signup",[
-  body('email','Please enter a valid email').isEmail(),
-  body('password','Password cannot be too short').isLength({min : 6})
-], signup);
+router.post(
+  "/signup",
+  [
+    body("email", "Please enter a valid email").isEmail(),
+    body("password", "Password cannot be too short").isLength({ min: 6 }),
+  ],
+  signup
+);
 
 // Route for sending OTP to the user's email
 router.post("/sendotp", sendotp);
 
 // Route for Changing the password
-router.post("/changepassword",[body('newPassword','Password cannot be too short').isLength({min : 6})], auth, changepassword);
+router.post(
+  "/changepassword",
+  [body("newPassword", "Password cannot be too short").isLength({ min: 6 })],
+  auth,
+  changepassword
+);
 
 // ********************************************************************************************************
 //                                      Reset Password
@@ -47,10 +63,17 @@ router.post("/changepassword",[body('newPassword','Password cannot be too short'
 router.post("/reset-password-token", resetPasswordToken);
 
 // Route for resetting user's password after verification
-router.post("/reset-password",[body('password','Password cannot be too short').isLength({min : 6})], resetPassword);
+router.post(
+  "/reset-password",
+  [body("password", "Password cannot be too short").isLength({ min: 6 })],
+  resetPassword
+);
 
 // Route for registering a meal
-router.post("/registerMeal",auth, registerMeal);
+router.post("/registerMeal", auth, registerMeal);
+
+router.post("/capturePayment", auth, capturePayment);
+router.post("/verifyPayment", auth, verifyPayment);
 
 // Export the router for use in the main application
 module.exports = router;
